90032b96d43367956a7ad63c63848cb96d9b5947
Clemens <clemens@muessener.com>
2014-08-14 15:40:04 +0200

    Closes connection

--- SQLite/src/de/duckout/connection/DatabaseConnection.java
+++ SQLite/src/de/duckout/connection/DatabaseConnection.java
@@ -4,15 +4,48 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
+/**
+ * @author Duckout.de
+ * Handles the SQLite Connection
+ */
 public class DatabaseConnection {
 	
-	public void establishConnection() throws ClassNotFoundException, SQLException{
+	/**
+	 * Establishes a standard SQLite Connection
+	 * @return Connection
+	 */
+	public Connection establishConnection(){
 		
-		//Get the JDBC driver
-		Class.forName("org.sqlite.JDBC");
+		try {
+			//Get the JDBC driver
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		
-		//Establish the database connection to database "duckout.db"
-		Connection connection = DriverManager.getConnection("jdbc:sqlite:duckout.db"); 
+		try {
+			//Establish the database connection to database "duckout.db"
+			Connection connection = DriverManager.getConnection("jdbc:sqlite:duckout.db");
+			return connection;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		//Return null in case Connection wasn't returned
+		return null;
+	}
+	
+	/**
+	 * Closes the SQL Connection
+	 * @param connection
+	 */
+	public void cloaseConnection(Connection connection){
+		try {
+			//Close SQL connection
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
